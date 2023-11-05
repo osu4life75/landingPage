8eebddbf255ac8aca4dcd9b71d1b3200c02f0b90
George Lynch <osu4life75@gmail.com>
2023-11-05 15:46:39 -0600

    Last Lesson

--- startup_files/script.js
+++ startup_files/script.js
@@ -1,12 +1,4 @@
-var  _ = require('lodash');
-console.log(_);
-import { without } from "lodash";
-
-console.log(without);
-
-
-
 let button = document.getElementById('myButton');
 button.addEventListener ('click', function(){
-    alert('More Info on The Way!!!');
+    console.log('button clicked');
 });
